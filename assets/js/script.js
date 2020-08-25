a2dc96bfc110cbb41cc46bf840106eca8f6aaac0
Tom Chestnut <tchestnut85@gmail.com>
2020-08-25 18:19:33 -0400

    added function to create list tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,17 @@ var createTaskHandler = function () {
 
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = taskNameInput;
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
 };
 
